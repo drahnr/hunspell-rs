2c33bcdd6f6d893a1397cae437db2a52d8c8274f
Lipka Boldizsár <lipkab@zoho.com>
2016-03-22 12:11:07 +0100

    Extern declarations for all Hunspell functions.

--- src/lib.rs
+++ src/lib.rs
@@ -25,4 +25,18 @@ extern {
     fn Hunspell_destroy(pHunspell: *mut Hunhandle);
 
     fn Hunspell_spell(pHunspell: *mut Hunhandle, word: *const i8) -> i32;
+    fn Hunspell_get_dic_encoding(pHunspell: *mut Hunhandle) -> *mut i8;
+
+    fn Hunspell_suggest(pHunspell: *mut Hunhandle, slst: *mut *mut *mut i8, word: *const i8) -> i32;
+    fn Hunspell_analyze(pHunspell: *mut Hunhandle, slst: *mut *mut *mut i8, word: *const i8) -> i32;
+    fn Hunspell_stem(pHunspell: *mut Hunhandle, slst: *mut *mut *mut i8, word: *const i8) -> i32;
+    fn Hunspell_stem2(pHunspell: *mut Hunhandle, slst: *mut *mut *mut i8, desc: *mut *mut i8, n: i32) -> i32;
+    fn Hunspell_generate(pHunspell: *mut Hunhandle, slst: *mut *mut *mut i8, word: *const i8, word2: *const i8) -> i32;
+    fn Hunspell_generate2(pHunspell: *mut Hunhandle, slst: *mut *mut *mut i8, word: *const i8, desc: *mut *mut i8, n: i32) -> i32;
+
+    fn Hunspell_add(pHunspell: *mut Hunhandle, word: *const i8) -> i32;
+    fn Hunspell_add_with_affix(pHunspell: *mut Hunhandle, word: *const i8, example: *const i8) -> i32;
+    fn Hunspell_remove(pHunspell: *mut Hunhandle, slst: *mut *mut *mut i8, n: i32) -> i32;
+
+    fn Hunspell_free_list(pHunspell: *mut Hunhandle, slst: *mut *mut *mut i8, n: i32);
 }
