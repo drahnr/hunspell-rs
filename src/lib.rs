630b6a54096f1429299c17106c0f24066731e143
Lipka Boldizsár <lipkab@zoho.com>
2016-03-22 17:16:18 +0100

    Hunspell struct, ctors & dtor.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,9 @@
 use std::ffi::CString;
 
 #[test]
-fn spell_check() {
-    unsafe {
-        let affpath = CString::new("/usr/share/hunspell/en_US.aff").unwrap();
-        let dicpath = CString::new("/usr/share/hunspell/en_US.dic").unwrap();
-        let good_word = CString::new("programming").unwrap();
-        let bad_word = CString::new("progrmaing").unwrap();
-        let spell = Hunspell_create(affpath.as_ptr(), dicpath.as_ptr());
-
-        assert!(Hunspell_spell(spell, good_word.as_ptr()) == 1);
-        assert!(Hunspell_spell(spell, bad_word.as_ptr()) == 0);
-
-        Hunspell_destroy(spell);
-    }
+fn create_and_destroy() {
+    let hs = Hunspell::new("/usr/share/hunspell/en_US.aff",
+                           "/usr/share/hunspell/en_US.dic");
 }
 
 enum Hunhandle {}
@@ -39,4 +29,40 @@ extern {
     fn Hunspell_remove(pHunspell: *mut Hunhandle, slst: *mut *mut *mut i8, n: i32) -> i32;
 
     fn Hunspell_free_list(pHunspell: *mut Hunhandle, slst: *mut *mut *mut i8, n: i32);
+}
+
+struct Hunspell {
+    handle: *mut Hunhandle
+}
+
+impl Hunspell {
+    pub fn new(affpath: &str, dicpath: &str) -> Hunspell {
+        let affpath = CString::new(affpath).unwrap();
+        let dicpath = CString::new(dicpath).unwrap();
+        unsafe {
+            Hunspell {
+                handle: Hunspell_create(affpath.as_ptr(), dicpath.as_ptr())
+            }
+        }
+    }
+
+    pub fn new_with_key(affpath: &str, dicpath: &str, key: &str) -> Hunspell {
+        let affpath = CString::new(affpath).unwrap();
+        let dicpath = CString::new(dicpath).unwrap();
+        let key = CString::new(key).unwrap();
+        unsafe {
+            Hunspell {
+                handle: Hunspell_create_key(affpath.as_ptr(), dicpath.as_ptr(),
+                                            key.as_ptr())
+            }
+        }
+    }
+}
+
+impl Drop for Hunspell {
+    fn drop(&mut self) {
+        unsafe {
+            Hunspell_destroy(self.handle);
+        }
+    }
 }
