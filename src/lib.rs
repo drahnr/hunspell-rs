089299e4106719bee98f6ab2f54ded824ba32445
Bernhard Schuster <bernhard@ahoi.io>
2020-05-15 12:01:59 +0200

    chore/license: yada yada Apache-.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
-// TODO move this into a separate crate
-// extracted from crate `hunspell` which is horribly outdated
-// and adapted to use hunspell-sys
+//   Copyright 2016 Lipka Boldizsár
+//   Copyright 2019 Alberto Piai
+//   Copyright 2020 Bernhard Schuster
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 
 use std::ffi::{CStr, CString};
 use std::ptr::null_mut;
